96249e5f7f6fdf1f8d1f0dc71939c2a10812c884
Łukasz Perski <pyra121@gmail.com>
2018-11-15 20:19:00 +0100

    Change project name

--- app/src/androidTest/java/pl/perski/lukasz/maraton/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/pl/perski/lukasz/maraton/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("pl.perski.lukasz.marathon", appContext.packageName)
+        assertEquals("pl.perski.lukasz.maraton", appContext.packageName)
     }
 }
--- app/src/main/java/pl/perski/lukasz/maraton/data/db/ExercisesDB.kt
+++ app/src/main/java/pl/perski/lukasz/maraton/data/db/ExercisesDB.kt
@@ -18,7 +18,7 @@ abstract class ExercisesDB : RoomDatabase() {
             if (INSTANCE == null) {
                 synchronized(ExercisesDB::class) {
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
-                            ExercisesDB::class.java, "marathon.db").allowMainThreadQueries()
+                            ExercisesDB::class.java, "maraton.db").allowMainThreadQueries()
                             .build()
                 }
             }
